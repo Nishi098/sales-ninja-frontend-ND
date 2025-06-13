c4a04638a481f54ca7af2904193490c99250f3b0
nishigandha dutta <nishigandha.dutta@gmail.com>
2025-06-13 06:30:20 +0200

    updated requirements file

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 from dateutil.parser import parse
-from dateutil.parser import parse
-import locale
-from io import StringIO
 import requests
 import datetime
 import matplotlib.pyplot as plt
